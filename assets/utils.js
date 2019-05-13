67254c39d72107d631766646f39f974936b8be13
Angelo <angelo@rcstart.it>
2019-05-13 22:09:36 +0200

    generate address

--- assets/utils.js
+++ assets/utils.js
@@ -5,23 +5,40 @@ import toDate from "date-fns/esm/toDate"
 import formatDistanceStrict from "date-fns/esm/formatDistanceStrict"
 import prettyNum from "pretty-num"
 import decode from "entity-decode"
+import CryptoJS from "crypto-js"
 
 export function generateAccount(mnemonic, password) {
   const seed = bip39.mnemonicToSeed(mnemonic, password)
+  const encryptedSeed = CryptoJS.AES.encrypt(seed.toString('hex'), password)
   const root = hdkey.fromMasterSeed(seed)
-  //const masterPrivateKey = root.privateKey.toString('hex')
   const addrNode = root.derive("m/44'/60'/0'/0/0")
   const pubKey = ethUtil.privateToPublic(addrNode._privateKey)
   const addr = ethUtil.publicToAddress(pubKey).toString('hex')
   const address = ethUtil.toChecksumAddress(addr)
 
+  //const decryptedSeed = decryptSeed(encryptedSeed.toString(), password).toString(CryptoJS.enc.Utf8)
+
   let account = {
-    address: address,
+    address: address.replace(address.charAt(0), "M"), // Minter address prefix
     mnemonic: mnemonic,
     password: password
   }
 
   return account
+}
+
+export function generateAccountFromDecryptedSeed(decryptedSeed) {
+  const root = hdkey.fromMasterSeed(Buffer.from(decryptedSeed, 'hex'))
+  const addrNode = root.derive("m/44'/60'/0'/0/0")
+  const pubKey = ethUtil.privateToPublic(addrNode._privateKey)
+  const addr = ethUtil.publicToAddress(pubKey).toString('hex')
+  const address = ethUtil.toChecksumAddress(addr)
+
+  return address.replace(address.charAt(0), "M")
+}
+
+export function decryptSeed(encryptedSeed, password) {
+  return CryptoJS.AES.decrypt(encryptedSeed, password)
 }
 
 export function getTimeDistance(timestamp) {
